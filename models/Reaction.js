42839d1175485b179a1ece10d3c59c3ac0e040b5
Kaila <kailaamia@gmail.com>
2024-02-01 15:13:53 -0600

    Added reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,31 @@
+const { Schema, model, default: mongoose } = require('mongoose');
+const dayjs = require('dayjs');
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: mongoose.ObjectId,
+        default: new ObjectId
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: dayjs(),
+        get: formatDate,
+    }
+})
+
+const formatDate = (date) => {
+    return date.format('ddd, MMM D, YYYY [at] hh:mm:ss a');
+}
+
+const Reaction = mongoose.model('reaction', reactionSchema);
+
+module.exports = Reaction;
--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction');
 const dayjs = require('dayjs');
 
 const thoughtSchema = new Schema(
@@ -20,12 +21,7 @@ const thoughtSchema = new Schema(
             type: String,
             required: true,
         },
-        reactions: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'reaction'
-            }
-        ]
+        reactions: [Reaction]
     },
     {
         toJSON: {
