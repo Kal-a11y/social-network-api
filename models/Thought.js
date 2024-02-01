3bc6ce3f92a56b45129b621752dfba87e535ed60
Kaila <kailaamia@gmail.com>
2024-02-01 10:14:02 -0600

    Added thought model

---
+++ models/Thought.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+const dayjs = require('dayjs');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            date: {
+                type: Date,
+                default: dayjs(),
+                get: formatDate,
+            }
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'reaction'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+)
+
+const formatDate = (date) => {
+    return date.format('ddd, MMM D, YYYY [at] hh:mm:ss a');
+}
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+})
+
+const Thought = mongoose.model('thought', thoughtSchema);
+
+module.exports = Thought;
+
